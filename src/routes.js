be17b615ff07db92e956a36493987b0cb7bfd468
GiovanneAndrade <vanne.as@hotmail.com>
2022-07-01 15:08:33 -0300

    atualizando

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import home from "./pages/exibicao/exibicao.js";
-import contact from "./pages/paginaInicial/paginaInicial.js";
-import about from "./pages/criacao/criacao.js";
-
-export default {
-  home: home(),
-  contact: contact(),
-  about: about()
-}
